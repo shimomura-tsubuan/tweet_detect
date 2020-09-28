d5dce65c007dd0895a69cc2d375fb57968c82478
shimomura.atsushi <26238953+momumomu@users.noreply.github.com>
2020-09-28 15:55:02 +0900

    first_commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# このリポジトリは？
+
+Twitterのつぶやきを監視して、SaaSの障害を検知する。  
+
+SaaS検知用としてますが、エゴサ用にも使えるかも。
+
+## 必要なもの
+
+1. Googleアカウント(G Suiteはもちろん、無料アカウントでもOK)
+2. Twitterアカウント
+3. Slackアカウント(無料アカウントでもOK)
+
+## 概要
+
+下記のようにして、SaaSの障害を検知する。
+
+1. スプレッドシートに障害検知したい「SaaS名」と「障害とみなすつぶやきのキーワード」を記載。
+   - 例：
+      - SaaS名： gmail
+      - 障害キーワード ： 落ちた
+2. Google Apps Scriptで上記1の文言をTwitter APIを利用してTwitterから検索する処理を定期実行する。
+   - このスクリプトの内容では15分間隔で実行させてます。
+3. 障害に該当するメッセージのつぶやき数が多い場合、Slackに通知。
+   - 短時間に数件の障害つぶやきがあれば、障害とみなしてSlackに通知するようにしています。理由は下記の通りです。
+      - 1-2件のつぶやきでは誤報の可能性が高いため。
+
+- 構成のイメージ図
+   - ![image.png](./img/image.png)
+
+## 初期構築前の事前準備
+
+1. Twitter APIの準備
+   - 下記手順などを参考に、Twitterの「API Key」と「API key secret」を取得。権限は「Read Only」だけ与えておけば十分です。
+     - https://help.twitter.com/ja/rules-and-policies/twitter-api
+     - https://qiita.com/nobu09/items/c940fc6e0d67ef1cbc85
+     - https://qiita.com/kngsym2018/items/2524d21455aac111cdee
+     - https://www.itti.jp/web-direction/how-to-apply-for-twitter-api/
+2. Slackチャンネル 及び そのチャンネルに対してのIncoming Webhook URLの準備
+   - 下記手順などを参考にIncoming WebhookのURLを取得する
+     - https://slack.com/intl/ja-jp/help/articles/115005265063-Slack-%E3%81%A7%E3%81%AE-Incoming-Webhook-%E3%81%AE%E5%88%A9%E7%94%A8
+     - https://qiita.com/kshibata101/items/0e13c420080a993c5d16
+
+## 初期構築手順
+
+1. このリポジトリにある「SAMPLE.xlsx」をGoogleDriveにアップロード。
+   - ※ファイル名はアップロード時に任意に変更頂いてOK。
+2. 1のファイルをスプレッドシート形式に変換。
+   - アップロード後に「アプリで開く」-「Google スプレッドシート」とすればOK。
+      - ![convert_ss.png](./img/convert_ss.png)
+3. スプレッドシートのスクリプトエディタを開く。
+   - 「ツール」-「スクリプトエディタ」とすればOK。
+      - ![open_scripteditor.png](./img/open_scripteditor.png)
+4. スクリプトエディタに「sample_gas.gs」の内容をコピペ。
+5. 2-5行目に事前準備で取得したキー情報を記載。
+   - 2行目： 変数「slackWebhookUrl」にSlackのIncoming WebhookのURLを記載。
+   - 4行目： 変数「twitter_api_key」にTwitterのAPI Keyを記載。
+   - 5行目： 変数「twitter_api_key_secret」にTwitterのAPI Key secretを記載。
+5. 15分おきにGASが実行されるようにトリガーを設定する。
+   - 「時計アイコン?」を選択 - 「トリガーを追加」を選択 - 「15分おき」に実行されるように設定。
+      - ![create_trigger_1.png](./img/create_trigger_1.png)
+      - ![create_trigger_2.png](./img/create_trigger_2.png)
+      - ![create_trigger_3.png](./img/create_trigger_3.png)
+
+## 運用手順
+
+下記内容をスプレッドシートのそれぞれのシートに記載すれば監視が始まります。
+
+1. 「SaaSリスト」シート
+   - A列: 監視したいSaaS名
+   - B列: Twitterに15分間の間に何件つぶやかれたらSlack通知するか、しきい値を記載。
+2. 「検知文言」シート
+   - A列： SaaSリストに記載された製品名と一緒に「何がつぶやかれたら」検知するかを記載。
+      - 例： 障害、落ちた、等
+      - 例： SaaSリストに「gmail」、検知文言に「落ちた」とした場合、「gmailが落ちたー」といったつぶやきを検知するようになります。
+3. 「無視文言」シート
+   - A列： つぶやきのうち無視する文言を記載。
+   - 例： 「連番」を記載した場合、「gmailで限定Boxの発送通知が来ない。当選落ちたか...」といったつぶやきを無視します。
+
+## 初期構築後のテスト方法
+
+1. 「SaaSリスト」シートにA列に「テスト」、B列に「1」と記載。
+2. 「検知文言」シートに「障害」と記載。
+3. Twitterで「障害テスト」とつぶやく
+4. 15分後にSlackに通知がくることを確認。
---
+++ sample_gas.js
@@ -0,0 +1,166 @@
+//参考にさせていただいたURL： https://qiita.com/nobu09/items/c940fc6e0d67ef1cbc85
+var slackWebhookUrl = 'https://hooks.slack.com/services/******/***********/**********';
+
+var twitter_api_key = '****';
+var twitter_api_key_secret = '****';
+
+function main(){
+  var check_range = new Date();
+  var check_range = check_range.setMinutes(check_range.getMinutes() - 15); //Tweet検索対象の分数を指定
+  
+  //スプレッドシートの情報を抽出
+  var ｓpreadSheetObject = SpreadsheetApp.getActiveSpreadsheet(); //現在アクティブなスプレッドシートを定義
+  //「SaaSリスト」シート
+  var list_sheetObject = ｓpreadSheetObject.getSheetByName('SaaSリスト'); //シート名を定義
+  var list_sheetArray = list_sheetObject.getDataRange().getValues(); // シートの情報を全て二次元配列に保管
+  //「検知文言」シート
+  var downlist_sheetObject = ｓpreadSheetObject.getSheetByName('検知文言'); //シート名を定義
+  var downlist_sheetArray = downlist_sheetObject.getDataRange().getValues(); // シートの情報を全て二次元配列に保管
+  //「無視文言」シート
+  var ignore_list_sheetObject = ｓpreadSheetObject.getSheetByName('無視文言'); //シート名を定義
+  var ignore_list_sheetArray = ignore_list_sheetObject.getDataRange().getValues(); // シートの情報を全て二次元配列に保管
+  
+  //「無視文言」シートから除外対象のTweet文言を抽出する
+  var ignore_regexp = return_ignore_regexp(ignore_list_sheetArray);
+
+  //スプレッドシートからTwitterの検索ログを抽出
+  for(var i=1; i<list_sheetArray.length; i++){ //1行目はヘッダーなので無視する。
+    var result_array = []; //配列を初期化
+    var search_key_1 = list_sheetArray[i][0]; //「SaaSリスト」シートのA列から検索対処のSaaS名を抽出
+    var threshold = list_sheetArray[i][1]; //「SaaSリスト」シートのB列からしきい値を抽出
+    
+    //「SaaSリスト」シートから抽出した内容と「検知文言」シートの内容をmergeしてTwitterから検索
+    for(var j=0; j<downlist_sheetArray.length; j++){
+      var search_key_2 = downlist_sheetArray[j][0];
+      var result_array = searchTweetsApps(result_array, search_key_1, search_key_2, check_range, ignore_regexp);
+      Utilities.sleep(100);
+    }
+
+    //エラー対策
+    if ( result_array == undefined ) { result_array = ["NULL"] }; //ヒットしなかった時用
+    var bool_1 = ( search_key_1 != "" ) && ( search_key_1 != undefined ); //スプレッドシートのセルが空欄だった時用(空欄ならfalseにする)
+    var bool_2 = ( search_key_2 != "" ) && ( search_key_2 != undefined );  //スプレッドシートのセルが空欄だった時用(空欄ならfalseにする)
+    
+    //検索日時の範囲に該当したログが指定件数以上あればSlack通知(1件だけのtweetとかだと誤検知の可能性もあるので)
+    var postNum = 10; //Slack通知する上限メッセージ数
+    if ( result_array.length > (threshold-1) && bool_1 && bool_2){
+      var summary_text = "「" + search_key_1 + "」で" + result_array.length + "件ヒットしました。(直近の" + String(postNum) + "件のみSlack通知します)";
+      postSlack(summary_text);
+      for(var k=0; k<result_array.length; k++){
+        postSlack(result_array[k]);
+        if (k==(postNum-1)){ break; }
+      }
+    }
+  }
+}
+
+function searchTweetsApps(result_array, app_name, detect_word, check_range, ignore_regexp) {
+  //引数
+  //arg1: 条件一致したTweet文を格納しておく配列
+  //arg2: Twitter検索したいSaaS名
+  //arg3: arg1に加えてand条件で検索したいキーワード
+  //arg4: 投稿日時の検知範囲
+  //arg5: 無視文言の正規表現パターンを指定
+  
+  if ( app_name == "" || app_name == undefined ) { return };
+  if ( detect_word == "" || detect_word == undefined ) { return };
+  
+  // ①Twitter Bearerトークンの取得（検索APIの呼び出しに必要）
+  // POST oauth2/token  https://developer.twitter.com/en/docs/basics/authentication/api-reference/token
+  var blob = Utilities.newBlob(twitter_api_key + ':' + twitter_api_key_secret);
+  var credential = Utilities.base64Encode(blob.getBytes());
+
+  var formData = {
+    'grant_type': 'client_credentials'
+  };
+
+  var basic_auth_header = {
+    'Authorization': 'Basic ' + credential
+  };
+
+  var options = {
+    'method': 'post',
+    'contentType': 'application/x-www-form-urlencoded;charset=UTF-8',
+    'headers':  basic_auth_header,
+    'payload': formData,
+  };
+
+  var oauth2_response = UrlFetchApp.fetch('https://api.twitter.com/oauth2/token', options);
+  var bearer_token = JSON.parse(oauth2_response).access_token; 
+
+  // ②Twitter 検索APIの呼び出し 
+  var bearer_auth_header = {
+    'Authorization': 'Bearer ' + bearer_token
+  };
+  
+  //Twitter検索
+  //var search_keyword = app_name + " -RT lang:ja";
+  var search_keyword = app_name + " " + detect_word + " -RT lang:ja";
+  var search_response = UrlFetchApp.fetch(
+    'https://api.twitter.com/1.1/search/tweets.json?q=' + search_keyword + '&lang=ja&result_type=recent&count=100',
+    { 'headers': bearer_auth_header });
+  var result = JSON.parse(search_response);
+  
+  //var result2 = []; //検索日時の範囲に合致するやつを格納する箱
+  result.statuses.forEach(function(status) {
+    var tweet_text = status.text;
+    var tweet_date = new Date(status.created_at);
+    var user_name = status.user.name;
+    
+    //Tweet文に求めている文言が含まれているか確認
+    //検索用の正規表現作成しとく
+    var app_name_regexp = new RegExp(".*" + app_name.toLowerCase() + ".*");
+    var detect_word_regexp = new RegExp(".*" + detect_word.toLowerCase() + ".*");
+    //Tweet文を精査
+    var tweet_text_2 = tweet_text.toLowerCase(); //tweet文言を小文字に変換しとく。
+    var bool_1 = app_name_regexp.test(tweet_text_2);
+    var bool_2 = detect_word_regexp.test(tweet_text_2);
+    var bool_3 = ( ! ignore_regexp.test(tweet_text_2) );
+    
+    //精査結果にマッチ かつ 検知範囲にマッチするものを抽出。
+    if ( check_range < tweet_date && bool_1 && bool_2 && bool_3 ) {
+      var jst = Utilities.formatDate(new Date(tweet_date), "JST", "yyyy/MM/dd HH:MM")
+      var tweet_data = "*" + user_name + "(" + jst + ")" + "*" + "\n";
+      var tweet_data = tweet_data + "```" + tweet_text + "```";
+      result_array.push(tweet_data)
+    }
+  });
+  
+  return result_array;
+}
+
+//Slack投稿するだけの関数
+function postSlack(text){
+  var data = {
+    "text": text
+  }
+  var options = {
+    'method' : 'post',
+    'contentType': 'application/json',
+    'payload' : JSON.stringify(data)
+  };
+  UrlFetchApp.fetch(slackWebhookUrl, options);
+}
+
+//正規表現オブジェクトに変換するだけの関数
+function return_ignore_regexp(ignore_list_sheetArray){
+  //「無視文言」リストに記載のある内容に.*を前後につける
+  var temp_array = []
+  for(var i=0; i<ignore_list_sheetArray.length; i++){
+    var ignore_word = ignore_list_sheetArray[i][0];
+    if ( ignore_word != "" && ignore_word != undefined ){
+      var ignore_word = ".*" + ignore_list_sheetArray[i][0] + ".*";
+      temp_array.push(ignore_word)
+    }
+  }
+  
+  //「|」で連結する。(空の場合はダミーの文字列を入れとく)
+  var temp_array_join = temp_array.join("|");
+  if ( temp_array_join == "" ){
+    var temp_array_join = "_N_U_L_L_";
+  }
+  
+  //正規表現オブジェクトにして返す
+  var regexp = new RegExp(temp_array_join);
+  return regexp
+}
